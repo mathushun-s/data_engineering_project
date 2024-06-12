523f0eb67e85230725ff8264f5eca0df96e8fec9
mathushun-s <58955594+mathushun-s@users.noreply.github.com>
2024-06-12 18:27:44 +0100

    Add files via upload

--- project.py
+++ project.py
@@ -1,25 +1,28 @@
-import numpy as np
 import csv
+import numpy as np
 import matplotlib.pyplot as plt
 import statistics as stats
 import collections
-from scipy.stats import norm
 
+#Saves the contents of targetA to a numpy array
 with open('targetA.csv', 'r') as f:
     reader = csv.reader(f)
     data = list(reader)
     targetA = np.array(data)
 f.close()
 
+#Saves the contents of targetB to a numpy array
 with open('targetB.csv', 'r') as f:
     reader = csv.reader(f)
     data = list(reader)
     targetB = np.array(data)
 f.close()
 
+#Assigns times in column B of targetA to an array
 timeA = targetA[1:, 1].astype(float)
 infoA = targetA[1:, 6]
 
+#Parses for sequence numbers in targetA and assigns to numpy array
 seqA = []
 
 for i in range(0,len(infoA)):
@@ -29,6 +32,7 @@ for i in range(0,len(infoA)):
   
 seqA = np.array(seqA)
 
+#Plotting sequence number over time graph
 plt.figure(figsize=(10, 6))
 plt.scatter(timeA, seqA)
 plt.xlabel('Time')
@@ -39,15 +43,18 @@ plt.close()
 
 packetLength = targetA[1:, 5].astype(int)
 
+#Here the statistics on packet length data is calculated
 mean = np.mean(packetLength)
 mode = stats.mode(packetLength)
 standardDev = np.std(packetLength)
 
+#The statistics are displayed to the user
 print("Statistics for packet lengths: ")
 print("Mean - " + str(int(mean)))
 print("Mode - " + str(mode))
 print("Standard Deviation - " + str(int(standardDev)))
 
+#Calculates probabilites for each packet length
 c = collections.Counter(packetLength)
 packets = []
 probabilities = []
@@ -57,6 +64,7 @@ for i in packetLength:
         count = c[i]
         probabilities.append(count/len(packetLength))
 
+#Plotting the packet length probability graph
 plt.figure(figsize=(10, 6))
 plt.bar(packets, probabilities)
 plt.xlabel('Packet Length')
@@ -72,6 +80,7 @@ combination = np.column_stack((x,y))
 combination = np.array(sorted(combination, key=lambda x: x[0], reverse=False))
 x, y = np.split(combination,2,axis=1)
 
+#Plotting the PDF graph
 plt.figure(figsize=(10, 6))
 plt.plot(x, y)
 plt.xlabel('Packet Length')
@@ -83,6 +92,7 @@ plt.close()
 
 cdf = np.cumsum(probabilities)
 
+#Plotting the CDF graph
 plt.figure(figsize=(10, 6))
 plt.plot(x, cdf)
 plt.xlabel('Packet Length')
@@ -92,51 +102,48 @@ plt.show()
 plt.close()
 
 timeB = targetB[1:, 1].astype(float)
-delays = []
-
 infoB = targetB[1:, 6]
-departureA = []
-arrivalB = []
+seqB = []
 
-'''seqB = []
-
-for j in range(len(targetB)-1):
-    n = infoB[j]
-    seq = n[n.index("Seq=") + len("Seq="):(n.index("Seq=")+9)]
+#Parses for sequence numbers in targetB
+for i in range(0,len(infoB)):
+    m = infoB[i]
+    seq = m[m.index("Seq=") + len("Seq="):(m.index("Seq=")+9)]
     seqB.append(int(seq))
-    times = np.where(seqA == int(seq))
-    departureA.append(timeA[times[0][0]])
-
+  
 seqB = np.array(seqB)
 
-for l in range(len(targetA)-1):
-    times = np.where(seqB == seqA[l])
-    print(seqB[times[0][0]])
-    print(seqA[l])
-    #arrivalB.append(timeB[times[0][0]])
+#This function removes any duplicate sequence numbers and leaves only the first instance in the array
+def removeDuplicates(seqs, times):
+    #Combining the times and sequence numbers into one 2D array
+    combo = np.column_stack((seqs,times))
+    combo = np.array(sorted(combo, key=lambda x: x[0], reverse=False))
+    _, indices = np.unique(combo[:, 0], return_index=True)
+    return combo[indices, :]
 
-print(len(departureA))
-print(len(arrivalB))   
-'''
+#Splitting the 2D array produced by the above function back to individual arrays
+seqA, timeA = np.split(removeDuplicates(seqA, timeA),2,axis=1)
+seqB, timeB = np.split(removeDuplicates(seqB, timeB),2,axis=1)
+
+#These arrays will store the data for the x-axis and y-axis, respectively, for the plot
 sequences = []
-for i in range(0,len(seqA)):
-    for j in range(0, len(infoB)):
-        n = infoB[j]
-        seqB = n[n.index("Seq=") + len("Seq="):(n.index("Seq=")+9)]
-        if seqA[i] == int(seqB):
-            sequences.append(int(seqB))
-            delay = abs(timeB[j] - timeA[i])
-            delays.append(delay)
-            
+delays = []
 
+print('The plot for the packet delays takes a while...')
+#Finding the packets in both targetA and targetB of the same sequence number
+for i in range(0, len(seqA)):
+    for j in range(0, len(seqB)):
+        if seqA[i] == seqB[j]:
+            sequences.append(seqA[i])
+            #Delay is calculated
+            delays.append(timeA[j] - timeB[i])
+            break
 
-delays = np.array(delays)
-sequences = np.array(sequences)
-
+#Plotting the packet delay graph
 plt.figure(figsize=(10, 6))
-plt.bar(sequences, delays)
-plt.xlabel('Sequence number')
-plt.ylabel('Time delay')
-plt.title('Time Delays of Packets')
+plt.plot(sequences, delays, marker='o', linestyle='', markersize=4)
+plt.title('Packet Delays from A to B')
+plt.xlabel('Sequence Number')
+plt.ylabel('Delay')
+plt.grid(True)
 plt.show()
-plt.close()

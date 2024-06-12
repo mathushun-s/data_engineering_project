f2951c80492e05e70a1d0cd78e022efb4a4c8a8c
mathushun-s <58955594+mathushun-s@users.noreply.github.com>
2024-06-12 18:24:20 +0100

    Add files via upload

--- project.py
+++ project.py
@@ -5,15 +5,12 @@ import statistics as stats
 import collections
 from scipy.stats import norm
 
-#reads data from csv files and puts it into numpy arrays
-#reads data from targetA.csv
 with open('targetA.csv', 'r') as f:
     reader = csv.reader(f)
     data = list(reader)
     targetA = np.array(data)
 f.close()
 
-#reads data from targetB.csv
 with open('targetB.csv', 'r') as f:
     reader = csv.reader(f)
     data = list(reader)
@@ -25,14 +22,13 @@ infoA = targetA[1:, 6]
 
 seqA = []
 
-#seperating the sequence numbers found in the targetA numpy array into a new array
 for i in range(0,len(infoA)):
     m = infoA[i]
     seq = m[m.index("Seq=") + len("Seq="):(m.index("Seq=")+9)]
     seqA.append(int(seq))
   
 seqA = np.array(seqA)
-#plotting scatter graph of sequence numbers of the packets in targetA against its time
+
 plt.figure(figsize=(10, 6))
 plt.scatter(timeA, seqA)
 plt.xlabel('Time')
@@ -43,18 +39,15 @@ plt.close()
 
 packetLength = targetA[1:, 5].astype(int)
 
-#calculates basic statistics(e.g mean, mode etc) of the packet lengths from targetA.csv
 mean = np.mean(packetLength)
 mode = stats.mode(packetLength)
 standardDev = np.std(packetLength)
 
-#displays the calculated statistics on command prompt
 print("Statistics for packet lengths: ")
 print("Mean - " + str(int(mean)))
 print("Mode - " + str(mode))
 print("Standard Deviation - " + str(int(standardDev)))
 
-#calculating the probabilities of the packet lengths
 c = collections.Counter(packetLength)
 packets = []
 probabilities = []
@@ -64,7 +57,6 @@ for i in packetLength:
         count = c[i]
         probabilities.append(count/len(packetLength))
 
-#plotting bar graph displaying the probabilities of packet length against the actual packet length
 plt.figure(figsize=(10, 6))
 plt.bar(packets, probabilities)
 plt.xlabel('Packet Length')
@@ -73,17 +65,29 @@ plt.title('Probabilities of packet length')
 plt.show()
 plt.close()
 
-#calculating and plotting the cumulative distribution function and the probability density function of packet lengths using the data in targetA
-N = len(packetLength) 
-data = np.random.randn(N) 
-count, bins_count = np.histogram(data, bins=10) 
+x = np.array(packets)
+y = np.array(probabilities)
 
-pdf = count/sum(count) 
-cdf = np.cumsum(pdf) 
+combination = np.column_stack((x,y))
+combination = np.array(sorted(combination, key=lambda x: x[0], reverse=False))
+x, y = np.split(combination,2,axis=1)
 
-plt.plot(bins_count[1:], pdf, color="red", label="PDF") 
-plt.plot(bins_count[1:], cdf, label="CDF")
-plt.legend()
+plt.figure(figsize=(10, 6))
+plt.plot(x, y)
+plt.xlabel('Packet Length')
+plt.ylabel('Probability')
+plt.title('PDF')
+plt.show()
+plt.close()
+
+
+cdf = np.cumsum(probabilities)
+
+plt.figure(figsize=(10, 6))
+plt.plot(x, cdf)
+plt.xlabel('Packet Length')
+plt.ylabel('Probability')
+plt.title('CDF')
 plt.show()
 plt.close()
 
@@ -94,14 +98,31 @@ infoB = targetB[1:, 6]
 departureA = []
 arrivalB = []
 
-#calculating the time delays of the packets departing from targetA and arriving in targetB
-#the packets in targetA and targetB are recognised by having the same sequence number 
+'''seqB = []
+
+for j in range(len(targetB)-1):
+    n = infoB[j]
+    seq = n[n.index("Seq=") + len("Seq="):(n.index("Seq=")+9)]
+    seqB.append(int(seq))
+    times = np.where(seqA == int(seq))
+    departureA.append(timeA[times[0][0]])
+
+seqB = np.array(seqB)
+
+for l in range(len(targetA)-1):
+    times = np.where(seqB == seqA[l])
+    print(seqB[times[0][0]])
+    print(seqA[l])
+    #arrivalB.append(timeB[times[0][0]])
+
+print(len(departureA))
+print(len(arrivalB))   
+'''
 sequences = []
 for i in range(0,len(seqA)):
     for j in range(0, len(infoB)):
         n = infoB[j]
         seqB = n[n.index("Seq=") + len("Seq="):(n.index("Seq=")+9)]
-        #checks if the sequence number for the packet in targetA matches with targetB
         if seqA[i] == int(seqB):
             sequences.append(int(seqB))
             delay = abs(timeB[j] - timeA[i])
@@ -112,7 +133,6 @@ for i in range(0,len(seqA)):
 delays = np.array(delays)
 sequences = np.array(sequences)
 
-#plotting the time delays against the sequence number
 plt.figure(figsize=(10, 6))
 plt.bar(sequences, delays)
 plt.xlabel('Sequence number')
